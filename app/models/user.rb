5dd329324dd9735aea8a4fdbe80bbb387a3132f2
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-10-27 13:37:14 +0000

    Fix linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
- devise :database_authenticatable, :registerable,
-				:recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :posts, foreign_key: 'author_id', primary_key: 'id'
   has_many :comments
