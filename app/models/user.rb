3dbac3d471b2525b9577a8ab3aaf1333dc891c31
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-10-28 13:25:48 +0000

    Add Endpoints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  include DeviseTokenAuth::Concerns::User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
